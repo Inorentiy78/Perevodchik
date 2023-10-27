5f7aed3ad4399cab20dbed7e25e04564dec3dd00
Inorentiy78 <148535452+Inorentiy78@users.noreply.github.com>
2023-10-27 17:00:31 +0500

    Create server.py

---
+++ server.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, jsonify
+import requests
+
+app = Flask(_name_)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/translate', methods=['POST'])
+def translate():
+    source_text = request.form['source_text']
+    source_lang = request.form['source_lang']
+    target_lang = request.form['target_lang']
+
+    if source_text == "":
+        return jsonify({'translation': ''})
+
+    url = 'https://translate.googleapis.com/translate_a/single?client=gtx&sl={}&tl={}&dt=t&q={}'.format(source_lang, target_lang, source_text)
+    response = requests.get(url)
+    data = response.json()
+    translation = data[0][0][0]
+
+    return jsonify({'translation': translation})
+
+if _name_ == '_main_':
+    app.run(debug=True)
